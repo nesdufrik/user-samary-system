227fab6daaf605c5d69ad3074846c5b57a858616
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-03-28 18:26:39 -0400

    Remove references to vue-html-to-paper library, update useEmpleados and useSucursales composables.

--- src/composables/useEmpleados.js
+++ src/composables/useEmpleados.js
@@ -1,40 +1,17 @@
 import { storeToRefs } from 'pinia'
 import { useEmpleadosStore } from '../stores/empleadosStore'
-import {
-    deleteEmpleado,
-    getEmpleados,
-    postEmpleado,
-    putEmpleado,
-} from '../helpers/helpEmpleados'
+import { getEmpleados, putEmpleado } from '../helpers/helpEmpleados'
 
 export const useEmpleados = () => {
     const empleadosStore = useEmpleadosStore()
 
-    const { empleadosArr, empleadoForm, actionState, errorApi } =
-        storeToRefs(empleadosStore)
-
-    const listEmpleados = async sucursalId => {
-        empleadosStore.loadEmpleados(await getEmpleados(sucursalId))
-    }
-
-    const updateEmpleado = async empleadoId => {
-        errorApi.value.show = false
-        actionState.value = true
-        empleadosStore.updtEmpleado(
-            await putEmpleado(empleadoForm.value, empleadoId)
-        )
-        actionState.value = false
-    }
+    const { actionState, errorApi } = storeToRefs(empleadosStore)
 
     return {
         //! propiedades
-        empleadosArr,
-        empleadoForm,
         actionState,
         errorApi,
 
         //! metodos
-        listEmpleados,
-        updateEmpleado,
     }
 }
--- src/composables/useSucursales.js
+++ src/composables/useSucursales.js
@@ -1,87 +1,16 @@
 import { storeToRefs } from 'pinia'
-import {
-    deleteSucursal,
-    getSucursal,
-    getSucursales,
-    postSucursal,
-    putSucursal,
-} from '../helpers/helpSucursales'
+import { getSucursal } from '../helpers/helpSucursales'
 import { useSucursalesStore } from '../stores/sucursalesStore'
 
 export const useSucursales = () => {
     const sucursalesStore = useSucursalesStore()
-    const {
-        sucursalData,
-        sucursalesArr,
-        sucursalForm,
-        actionSucursal,
-        loadedSucursales,
-        errorApi,
-    } = storeToRefs(sucursalesStore)
-
-    const listSucursales = async id => {
-        sucursalesStore.loadSucursales(await getSucursales(id))
-        loadedSucursales.value = true
-    }
-
-    const newSucursal = async id => {
-        actionSucursal.value = true
-        errorApi.value.show = false
-        sucursalesStore.addSucursal(await postSucursal(sucursalForm.value, id))
-        actionSucursal.value = false
-    }
-
-    const editarSucursal = () => {
-        sucursalesStore.editSucursal()
-    }
-
-    const updateSucursal = async id => {
-        actionSucursal.value = true
-        sucursalesStore.updtSucursal(await putSucursal(sucursalForm.value, id))
-        actionSucursal.value = false
-    }
-
-    const delSucursal = async id => {
-        actionSucursal.value = true
-        await deleteSucursal(id)
-        sucursalesStore.deleleSucursal(id)
-        actionSucursal.value = false
-    }
-
-    const loadSucursal = async id => {
-        if (!loadedSucursales.value) {
-            sucursalesStore.loadSucursalData(await getSucursal(id))
-            loadedSucursales.value = true
-            return
-        }
-        sucursalesStore.findSucursalData(id)
-    }
-
-    const cleanForm = () => {
-        sucursalForm.value = {}
-        errorApi.value = {}
-    }
-
-    const cleanAndRedirect = dir => {
-        sucursalesStore.redirect(dir)
-    }
+    const { sucursalData, errorApi } = storeToRefs(sucursalesStore)
 
     return {
         //! propiedades
         sucursalData,
-        sucursalesArr,
-        sucursalForm,
-        actionSucursal,
         errorApi,
 
         //! metodos
-        listSucursales,
-        newSucursal,
-        editarSucursal,
-        updateSucursal,
-        delSucursal,
-        loadSucursal,
-        cleanForm,
-        cleanAndRedirect,
     }
 }
