515d7d6e554bfb26329bee2c61677a12d699f283
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-04-09 14:23:34 -0400

    Update App.vue to implement Suspense, KeepAlive, and loading state.

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,47 @@
 <template>
-    <!-- <div class="vh-100" v-if="appLoading">
-        <div class="d-flex flex-column text-center h-100">
-            <main class="w-100 m-auto flex-shrink-0 p-5">
-                <h1 class="display-3 fw-bold text-success">Cargando</h1>
-                <div
-                    class="spinner-grow text-success"
-                    style="width: 3rem; height: 3rem"
-                    role="status"
-                ></div>
-            </main>
-        </div>
-    </div>
-    <div class="vh-100" v-else>
-        <RouterView />
-    </div> -->
-    <Suspense>
-        <template #default>
-            <div class="vh-100">
-                <RouterView />
-            </div>
+    <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+            <Transition mode="out-in">
+                <KeepAlive>
+                    <Suspense>
+                        <!-- main content -->
+                        <div class="vh-100">
+                            <component :is="Component"></component>
+                        </div>
+
+                        <!-- loading state -->
+                        <template #fallback>
+                            <div class="vh-100">
+                                <div
+                                    class="d-flex flex-column text-center h-100"
+                                >
+                                    <main
+                                        class="w-100 m-auto flex-shrink-0 p-5"
+                                    >
+                                        <h1
+                                            class="display-3 fw-bold text-success"
+                                        >
+                                            Cargando
+                                        </h1>
+                                        <div
+                                            class="spinner-grow text-success"
+                                            style="width: 3rem; height: 3rem"
+                                            role="status"
+                                        ></div>
+                                    </main>
+                                </div>
+                            </div>
+                        </template>
+                    </Suspense>
+                </KeepAlive>
+            </Transition>
         </template>
-        <template #fallback>
-            <div class="vh-100">
-                <div class="d-flex flex-column text-center h-100">
-                    <main class="w-100 m-auto flex-shrink-0 p-5">
-                        <h1 class="display-3 fw-bold text-success">Cargando</h1>
-                        <div
-                            class="spinner-grow text-success"
-                            style="width: 3rem; height: 3rem"
-                            role="status"
-                        ></div>
-                    </main>
-                </div>
-            </div>
-        </template>
-    </Suspense>
+    </RouterView>
 </template>
 
 <script setup>
-import { useAuth } from './composables/useAuth'
-
-const { appLoading } = useAuth()
+// import { useAuth } from './composables/useAuth'
+// const { appLoading } = useAuth()
 </script>
 
 <style lang="scss" scoped></style>
