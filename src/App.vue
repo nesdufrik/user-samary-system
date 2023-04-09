c76f93b1c16f5fcc286d1fe60285e0f0bf939b31
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-04-09 14:29:49 -0400

    Update App.vue to add loading state and improve structure.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
 <template>
-    <RouterView v-slot="{ Component }">
-        <template v-if="Component">
-            <Transition mode="out-in">
-                <KeepAlive>
-                    <Suspense>
-                        <!-- main content -->
-                        <div class="vh-100">
-                            <component :is="Component"></component>
-                        </div>
+    <div class="vh-100">
+        <RouterView v-slot="{ Component }">
+            <template v-if="Component">
+                <Suspense>
+                    <!-- main content -->
 
-                        <!-- loading state -->
-                        <template #fallback>
-                            <div class="vh-100">
+                    <component :is="Component"></component>
+
+                    <!-- loading state -->
+                    <template #fallback>
+                        <div class="d-flex flex-column text-center h-100">
+                            <main class="w-100 m-auto flex-shrink-0 p-5">
+                                <h1 class="display-3 fw-bold text-success">
+                                    Cargando
+                                </h1>
                                 <div
-                                    class="d-flex flex-column text-center h-100"
-                                >
-                                    <main
-                                        class="w-100 m-auto flex-shrink-0 p-5"
-                                    >
-                                        <h1
-                                            class="display-3 fw-bold text-success"
-                                        >
-                                            Cargando
-                                        </h1>
-                                        <div
-                                            class="spinner-grow text-success"
-                                            style="width: 3rem; height: 3rem"
-                                            role="status"
-                                        ></div>
-                                    </main>
-                                </div>
-                            </div>
-                        </template>
-                    </Suspense>
-                </KeepAlive>
-            </Transition>
-        </template>
-    </RouterView>
+                                    class="spinner-grow text-success"
+                                    style="width: 3rem; height: 3rem"
+                                    role="status"
+                                ></div>
+                            </main>
+                        </div>
+                    </template>
+                </Suspense>
+            </template>
+        </RouterView>
+    </div>
 </template>
 
-<script setup>
-// import { useAuth } from './composables/useAuth'
-// const { appLoading } = useAuth()
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
